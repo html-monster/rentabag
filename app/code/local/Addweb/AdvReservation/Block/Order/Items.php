b6d2c10f63a322d92c470478e6f55c1c55c5f5a7
vlasakh <vladimir.saxarov@yandex.ua>
2017-05-12 06:46:36 +0300

    Поправил ошибку со статусом заказа

--- app/code/local/Addweb/AdvReservation/Block/Order/Items.php
+++ app/code/local/Addweb/AdvReservation/Block/Order/Items.php
@@ -13,24 +13,25 @@ class Addweb_AdvReservation_Block_Order_Items extends Mage_Sales_Block_Items_Abs
     {
         $order = Mage::registry('current_order');
 
+        $result = [];
+        if( $order->getData()['state'] == 'processing' )
+        {
+            // *** Get prod pledge payment info ***
+            $resource = Mage::getSingleton('core/resource');
+            $readConnection = $resource->getConnection('core_read');
 
-        // *** Get prod pledge payment info ***
-        $resource = Mage::getSingleton('core/resource');
-        $readConnection = $resource->getConnection('core_read');
-
-        $query = "SELECT
+            $query = "SELECT
                   id, mailletters, price, id_prod,
                   DATE_FORMAT(fdate, '%Y-%m-%d') fdate,
                   DATE_FORMAT(tdate, '%Y-%m-%d') tdate
                 FROM adv_rent
                 WHERE id_order = {$this->getRequest()->getParams()['order_id']} 
                   AND mailletters < 4";
-        $results = $readConnection->fetchAll($query);
+            $results = $readConnection->fetchAll($query);
 
-        foreach ($results as $key => $val)
-        {
-            $rentInfo[$val['id_prod']] = $val;
-        } // end foreach
+            foreach ($results as $key => $val) {
+                $rentInfo[$val['id_prod']] = $val;
+            } // end foreach
 
 //
 //        // process orders product without pledge
@@ -52,33 +53,28 @@ class Addweb_AdvReservation_Block_Order_Items extends Mage_Sales_Block_Items_Abs
                 ->load();
 
 
-            $result = false;
-//            $result[$order_id] = ['id' => $order->getId()];
-            foreach ($orderItems as $Item)
-            {
+            foreach ($orderItems as $Item) {
                 $Item = Mage::getModel('catalog/product')->setStoreId($Item->getStoreId())->load($Item->getProductId());
                 // check each product
-                if( ($id = $Item->getId()) && count($rentInfo[$id]) )
-                {
+                if (($id = $Item->getId()) && count($rentInfo[$id])) {
                     $datetime1 = new DateTime("now");
                     $datetime2 = (new DateTime())->createFromFormat('Y-m-d', $rentInfo[$id]['fdate']);
-//                    $interval = $datetime1->diff($datetime2);
 
-                    if( $datetime1->getTimestamp() < $datetime2->getTimestamp() )
-                    {
+                    if ($datetime1->getTimestamp() < $datetime2->getTimestamp()) {
                         $itemData = $Item->getData();
 
                         $result[$id] = ['name' => $Item->getName(),
                             'url' => $Item->getProductUrl(),
                             'img' => $Item->getImageUrl(),
-                            'pledge' => number_format($itemData['pledge'], 2 ),
+                            'pledge' => number_format($itemData['pledge'], 2),
                             'fdate' => date('d M Y', strtotime($rentInfo[$id]['fdate'])),
-                            'price' => number_format($rentInfo[$id]['price'], 2 ),
+                            'price' => number_format($rentInfo[$id]['price'], 2),
                         ];
                     } // endif
                 }
             }
-//        }
+        } // endif
+
 
         return [$order, $result];
     }
