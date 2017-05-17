8387101ebd928c64953c244e93df839fbf6ed24d
Vladimir <Zotaper@yandex.ru>
2017-05-17 05:55:14 +0300

    Добавил тестовую форму в заказ

--- app/code/local/Addweb/AdvReservation/Block/Order/Items.php
+++ app/code/local/Addweb/AdvReservation/Block/Order/Items.php
@@ -81,11 +81,59 @@ class Addweb_AdvReservation_Block_Order_Items extends Mage_Sales_Block_Items_Abs
 
 
 
-    function getSagePayPledgeForm()
+    function getSagePayPledgeFormData()
     {
-        $pass = Mage::getStoreConfig('addweb/test_pass', 1);
-        $Helper = Mage::helper('advreservation')->test();
+        $CryptInfo = [
+            'VendorTxCode' => 'rentabag-DEFERRED-10000084-3-Test',
+            'Amount' => 350,
+            'Currency' => 'GBP',
+            'Description' => 'Some payment test 100084',
+//            'SuccessURL' => 'http://rentabag.dev/test/test.php?success=1',
+            'SuccessURL' => 'http://rentabagdev.rucheek.in.ua/test/test.php?success=1',
+            'FailureURL' => 'http://rentabagdev.rucheek.in.ua/test/test.php?fail=1',
+//            'FailureURL' => 'http://rentabag.dev/test/test.php?fail=1',
+            'CustomerName' => 'Fedor Boyarin',
+            'CustomerEMail' => 'fe@boya.rin',
 
-        return $Helper;
+            'VendorEMail' => '',
+            'SendEMail' => '0',
+            'eMailMessage' => '',
+            'ApplyAVSCV2' => '0',
+            'Apply3DSecure' => '0',
+            'AllowGiftAid' => '0',
+            'BillingAgreement' => '1',
+            'VendorData' => 'some my vendor data',
+            'BillingAddress2' => 'Frunse 55',
+            'BillingPhone' => '44 (0)7933 000 000',
+            'BillingState' => '',
+            'DeliveryState' => '',
+            'DeliveryPhone' => '44 (0)7933 000 000',
+            'BasketXML' => '',
+
+            'BillingSurname' => 'Boyarin',
+            'BillingFirstnames' => 'Fedorka',
+            'BillingAddress1' => 'Frunse 55',
+            'BillingCity' => 'Pervomaisk',
+//            'BillingPostCode' => '39610',
+            'BillingPostCode' => 'W1A 1BL',
+            'BillingCountry' => 'GB',
+            'DeliverySurname' => 'Boyarin',
+            'DeliveryFirstnames' => 'Fedorka',
+            'DeliveryAddress1' => 'Frunse 55',
+            'DeliveryCity' => 'Pervomaisk',
+            'DeliveryPostCode' => '39610',
+            'DeliveryCountry' => 'GB',
+        ];
+//VendorTxCode=rentabag-DEFERRED-1494902510-255249023&Amount=57.29&Currency=GBP&Description=DVDs from Sagepay Demo Page&CustomerName=Fname Mname Surname&CustomerEMail=customer@example.com&VendorEMail=&SendEMail=0&eMailMessage=&BillingSurname=Surname&BillingFirstnames=Fname Mname&BillingAddress1=BillAddress Line 1&BillingAddress2=BillAddress Line 2&BillingCity=BillCity&BillingPostCode=W1A 1BL&BillingCountry=GB&BillingPhone=44 (0)7933 000 000&ApplyAVSCV2=0&Apply3DSecure=0&AllowGiftAid=1&BillingAgreement=1&BillingState=&customerEmail=customer@example.com&DeliveryFirstnames=Fname Mname&DeliverySurname=Surname&DeliveryAddress1=BillAddress Line 1&DeliveryAddress2=BillAddress Line 2&DeliveryCity=BillCity&DeliveryPostCode=W1A 1BL&DeliveryCountry=GB&DeliveryState=&DeliveryPhone=44 (0)7933 000 000&VendorData=some my vendor data&BasketXML=<basket><item><description>Batman - The Dark Knight</description><productSku>DVD2SKU</productSku><productCode>9256370</productCode><quantity>1</quantity><unitNetAmount>10.99</unitNetAmount><unitTaxAmount>0.50</unitTaxAmount><unitGrossAmount>11.49</unitGrossAmount><totalGrossAmount>11.49</totalGrossAmount></item><item><description>IronMan</description><productSku>DVD3SKU</productSku><productCode>84661832</productCode><quantity>5</quantity><unitNetAmount>8.75</unitNetAmount><unitTaxAmount>0.10</unitTaxAmount><unitGrossAmount>8.85</unitGrossAmount><totalGrossAmount>44.25</totalGrossAmount></item><deliveryNetAmount>1.50</deliveryNetAmount><deliveryTaxAmount>0.05</deliveryTaxAmount><deliveryGrossAmount>1.55</deliveryGrossAmount></basket>&SurchargeXML=<surcharges><surcharge><paymentType>MC</paymentType><percentage>5</percentage></surcharge><surcharge><paymentType>VISA</paymentType><fixed>3.5</fixed></surcharge></surcharges>&SuccessURL=http://rentabag.dev/VspPHPKit/demo/form/success&FailureURL=http://rentabag.dev/VspPHPKit/demo/form/failure
+
+        $pass = Mage::getStoreConfig('addweb/test_pass', 1);
+
+        return [
+            'data' => ['VPSProtocol' => '3.00',
+                'TxType' => 'DEFERRED',
+                'Vendor' => 'rentabag',
+                'Crypt' => Mage::helper('advreservation')->encryptAes(Mage::helper('advreservation')->arrayToQueryString($CryptInfo), $pass),
+            ]
+        ];
     }
 }
--- app/code/local/Addweb/AdvReservation/Helper/Data.php
+++ app/code/local/Addweb/AdvReservation/Helper/Data.php
@@ -11,4 +11,76 @@ class Addweb_AdvReservation_Helper_Data extends Mage_Core_Helper_Abstract
     {
         return "hello";
     }
+
+
+
+    /**
+     * Using for prepare crypt string
+     * @param array $data
+     * @param string $delimiter
+     * @param bool $urlencoded
+     * @return bool|string
+     */
+    static public function arrayToQueryString(array $data, $delimiter = '&', $urlencoded = false)
+    {
+        $queryString = '';
+        $delimiterLength = strlen($delimiter);
+
+        // Parse each value pairs and concate to query string
+        foreach ($data as $name => $value)
+        {
+            // Apply urlencode if it is required
+            if ($urlencoded)
+            {
+                $value = urlencode($value);
+            }
+            $queryString .= $name . '=' . $value . $delimiter;
+        }
+
+        // remove the last delimiter
+        return substr($queryString, 0, -1 * $delimiterLength);
+    }
+
+
+
+    /**
+     * Crypt string for Sage Pay
+     * @param $string
+     * @param $key
+     * @return string
+     */
+    public function encryptAes($string, $key)
+    {
+        // AES encryption, CBC blocking with PKCS5 padding then HEX encoding.
+        // Add PKCS5 padding to the text to be encypted.
+        $string = $this->addPKCS5Padding($string);
+
+        // Perform encryption with PHP's MCRYPT module.
+        $crypt = mcrypt_encrypt(MCRYPT_RIJNDAEL_128, $key, $string, MCRYPT_MODE_CBC, $key);
+
+        // Perform hex encoding and return.
+        return "@" . strtoupper(bin2hex($crypt));
+    }
+
+
+
+    /**
+     * PHP's mcrypt does not have built in PKCS5 Padding, so we use this.
+     * @param string $input The input string.
+     * @return string The string with padding.
+     */
+    protected function addPKCS5Padding($input)
+    {
+        $blockSize = 16;
+        $padd = "";
+
+        // Pad input to an even block size boundary.
+        $length = $blockSize - (strlen($input) % $blockSize);
+        for ($i = 1; $i <= $length; $i++)
+        {
+            $padd .= chr($length);
+        }
+
+        return $input . $padd;
+    }
 }
