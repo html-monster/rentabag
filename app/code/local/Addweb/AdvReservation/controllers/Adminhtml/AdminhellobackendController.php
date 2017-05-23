5dcc39eba68f1b97297f52a1518bcb25d3e6f75c
vlasakh <vladimir.saxarov@yandex.ua>
2017-05-23 21:02:32 +0300

    Создаю контроллер в админке

---
+++ app/code/local/Addweb/AdvReservation/controllers/Adminhtml/AdminhellobackendController.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tianna
+ * Date: 23.05.17
+ * Time: 10:11
+ */
+
+//class Addweb_AdvReservation_Adminhtml_AdminhellobackendController extends Mage_Adminhtml_Controller_Action
+class Addweb_AdvReservation_AdminhellobackendController extends Mage_Adminhtml_Controller_Action
+{
+    public function pledgenotifyemailAction()
+    {
+        $aa = "here";
+
+        $this->loadLayout();
+        $this->renderLayout();
+    }
+}
