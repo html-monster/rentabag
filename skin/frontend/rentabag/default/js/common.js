cfbf4d411b25abab917f53fd33db25b69e0ae0f1
altbet <gadget.letnik@gmail.com>
2017-07-06 23:23:46 +0300

    убрал, чтобы значение не могло стоять в занятой зоне дат

--- skin/frontend/rentabag/default/js/common.js
+++ skin/frontend/rentabag/default/js/common.js
@@ -201,6 +201,8 @@ $j(document).ready(function () {
 
   var today_unix = +moment(moment(today, 'DD/MM/YYYY')).format('x');
 
+  var endDate = today_unix;
+
   var some_date_range = G_VARS.rentRanges;
 
   var input_date = $j('input[name="daterange"]');
@@ -223,7 +225,7 @@ $j(document).ready(function () {
 
     "minDate": today,
 
-    "endDate": moment(today_unix + 86400000).format('DD/MM/YYYY'),
+    "endDate": moment(endDate).format('DD/MM/YYYY'),
 
     "dateLimit": { days: 13 },
 
